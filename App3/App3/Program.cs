3e124f635ea90e351fed4e5c4a08c56d7dbec40e
Ugnius <ugniusnaujokas@gmail.com>
2019-12-24 05:56:26 +0200

    Completed App3

--- App3/App3/Program.cs
+++ App3/App3/Program.cs
@@ -10,6 +10,72 @@ namespace App3
     {
         static void Main(string[] args)
         {
+            bool skaiciaustikrinimasA;
+            double skaiciusA;
+            bool skaiciaustikrinimasB;
+            double skaiciusB;
+            string atsakymas;
+            for (; ; )
+            {
+                Console.WriteLine("Iveskite pirma kintamaji: ");
+                String intLaik = Console.ReadLine();
+                skaiciaustikrinimasA = double.TryParse(intLaik, out skaiciusA);
+                if (skaiciaustikrinimasA == true)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Neteisingas ivesto skaiciaus formatas");
+                }
+
+            }
+
+            for (; ; )
+            {
+                Console.WriteLine("Iveskite antra kintamaji: ");
+                String intLaik = Console.ReadLine();
+                skaiciaustikrinimasB = double.TryParse(intLaik, out skaiciusB);
+                if (skaiciaustikrinimasB == true)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Neteisingas ivesto skaiciaus formatas");
+                }
+
+            }
+
+            for (; ; )
+            {
+                Console.WriteLine("Iveskite operacija(+,-,*,/):");
+                String operacija = Console.ReadLine();
+
+                switch (operacija)
+                {
+                    case "+":
+                        Console.WriteLine("{0} + {1} = {2}", skaiciusA, skaiciusB, skaiciusA + skaiciusB);
+                        break;
+                    case "-":
+                        Console.WriteLine("{0} - {1} = {2}",skaiciusA,skaiciusB,skaiciusA - skaiciusB);
+                        break;
+                    case "*":
+                        Console.WriteLine("{0} * {1} = {2}", skaiciusA, skaiciusB, skaiciusA * skaiciusB);
+                        break;
+                    case "/":
+                        Console.WriteLine("{0} / {1} = {2}", skaiciusA, skaiciusB, skaiciusA / skaiciusB);
+                        break;
+                }
+                Console.WriteLine("Ar noresite atlikti dar viena veiksma?");
+                atsakymas = Console.ReadLine();
+                if(atsakymas == "ne")
+                {
+                    break;
+                }
+            }
+            Console.ReadLine();
+
         }
     }
 }
