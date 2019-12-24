70fb4d960873d3f18a9f946ccd4e81df79daca4e
Ugnius <ugniusnaujokas@gmail.com>
2019-12-24 05:42:42 +0200

    Completed App2

--- App2/App2/Program.cs
+++ App2/App2/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace App2
+namespace App1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            bool skaiciaustikrinimasA;
+            double skaiciusA;
+            for (; ; )
+            {
+                Console.WriteLine("Iveskite pirma kintamaji: ");
+                String intLaik = Console.ReadLine();
+                skaiciaustikrinimasA = double.TryParse(intLaik, out skaiciusA);
+                if (skaiciaustikrinimasA == true)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Neteisingas ivesto skaiciaus formatas");
+                }
+
+            }
+
+            var teigiamasTikrinimas = (skaiciusA > 0);
+            var lyginisTikrinimas = (skaiciusA % 2  == 0);
+
+
+            Console.WriteLine("Ar skaicius {0} yra teigimas? {1}", skaiciusA, teigiamasTikrinimas);
+            Console.WriteLine("Ar skaicius {0} yra lyginis? {1}", skaiciusA, lyginisTikrinimas);
+
+
+            Console.ReadLine();
+
         }
     }
 }
