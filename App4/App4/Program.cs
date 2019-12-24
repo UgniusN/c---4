ce8da9e7968b31e9db08af1f28e35beb5f857277
Ugnius <ugniusnaujokas@gmail.com>
2019-12-24 06:04:19 +0200

    Completed App4

---
+++ App4/App4/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool skaiciaustikrinimas;
+            double skaiciusA, skaiciusB, skaiciusC;
+            String intLaik;
+
+            for (; ; )
+            {
+                Console.WriteLine("Iveskite pirma kintamaji: ");
+                intLaik = Console.ReadLine();
+                skaiciaustikrinimas = double.TryParse(intLaik, out skaiciusA);
+                if (skaiciaustikrinimas== true)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Neteisingas ivesto skaiciaus formatas");
+                }
+            }
+
+            for (; ; )
+            {
+                Console.WriteLine("Iveskite antra kintamaji: ");
+                intLaik = Console.ReadLine();
+                skaiciaustikrinimas = double.TryParse(intLaik, out skaiciusB);
+                if (skaiciaustikrinimas == true)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Neteisingas ivesto skaiciaus formatas");
+                }
+            }
+
+            for (; ; )
+            {
+                Console.WriteLine("Iveskite trecia kintamaji: ");
+                intLaik = Console.ReadLine();
+                skaiciaustikrinimas = double.TryParse(intLaik, out skaiciusC);
+                if (skaiciaustikrinimas == true)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Neteisingas ivesto skaiciaus formatas");
+                }
+            }
+
+            double maxSkaicius = Math.Max(Math.Max(skaiciusA, skaiciusB), skaiciusC);
+
+            Console.WriteLine("Didziausias is siu ivestu skaiciu yra: {0}", maxSkaicius);
+
+            Console.Read();
+        }
+    }
+}
