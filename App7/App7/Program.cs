6fc17377e81e22594d4d86ac5c6b0ce7a683602e
Ugnius <ugniusnaujokas@gmail.com>
2019-12-24 06:26:03 +0200

    Completed App7

--- App7/App7/Program.cs
+++ App7/App7/Program.cs
@@ -10,6 +10,50 @@ namespace App7
     {
         static void Main(string[] args)
         {
+            bool skaiciaustikrinimas;
+            int savaitesDiena;
+            String intLaik;
+
+            for (; ; )
+            {
+                Console.WriteLine("Iveskite savaites dienos skaiciu: ");
+                intLaik = Console.ReadLine();
+                skaiciaustikrinimas = int.TryParse(intLaik, out savaitesDiena);
+                if (skaiciaustikrinimas == true && savaitesDiena > 0 && savaitesDiena < 8)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Neteisingai ivestas savaies dienos skaiciaus formatas, teisingas ( 1 - 7 )");
+                }
+            }
+            switch(savaitesDiena)
+            {
+                case 1:
+                    Console.WriteLine("{0} savaites diena yra Pirmadienis",savaitesDiena);
+                    break;
+                case 2:
+                    Console.WriteLine("{0} savaites diena yra Antradienis", savaitesDiena);
+                    break;
+                case 3:
+                    Console.WriteLine("{0} savaites diena yra Treciadienis", savaitesDiena);
+                    break;
+                case 4:
+                    Console.WriteLine("{0} savaites diena yra Ketvirtadienis", savaitesDiena);
+                    break;
+                case 5:
+                    Console.WriteLine("{0} savaites diena yra Penktadienis", savaitesDiena);
+                    break;
+                case 6:
+                    Console.WriteLine("{0} savaites diena yra Sestadienis", savaitesDiena);
+                    break;
+                case 7:
+                    Console.WriteLine("{0} savaites diena yra Sekmadienis", savaitesDiena);
+                    break;
+            }
+
+            Console.Read();
         }
     }
 }
