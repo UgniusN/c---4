93ba42a6e96b924a9b169221a957e98ffe2b53c3
Ugnius <ugniusnaujokas@gmail.com>
2019-12-24 05:23:38 +0200

    Completed App1

--- App1/App1/Program.cs
+++ App1/App1/Program.cs
@@ -6,7 +6,20 @@ namespace App1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Iveskite pirma kintamaji: ");
+            String intLaik = Console.ReadLine();
+            int firstInteger = Convert.ToInt32(intLaik);
+
+            Console.WriteLine("Iveskite antra kintamaji: ");
+            String intLaikAntr = Console.ReadLine();
+            int secondInteger = Convert.ToInt32(intLaikAntr);
+
+            var tikrinimas = (firstInteger == secondInteger);
+
+            Console.WriteLine("Ar vienodi? {0}", tikrinimas);
+
+            Console.ReadLine();
+
         }
     }
 }
